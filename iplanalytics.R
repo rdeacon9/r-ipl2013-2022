f9148034001e59b43eabc062ebfeefe55f360f56
rdeacon9 <ryan.deacon9@gmail.com>
2022-09-01 13:31:26 +0200

    Update iplanalytics.R

--- iplanalytics.R
+++ iplanalytics.R
@@ -1,1 +1,6 @@
-setwd("C:\\Users\\Ryan\\Documents\\GitHub\\")
+auction_data <- read.csv("IPLPlayerAuctionData.csv")
+standing_path <- "table export.xlsx"
+
+#coverting indian Rupee to EUR
+auction_data["Euro"] <- auction_data$Amount * 0.012
+
